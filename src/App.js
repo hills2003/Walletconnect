bb0d577e53a1d2d70dd71c955ae4c99dd862c0d2
=hillary <=chillaryvictor@gmail.com>
2021-08-09 19:44:01 +0100

    wallet connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { useEffect } from 'react';
+import Web3 from "web3";
+
+//  Create Web3 instance
 
 function App() {
+
+
+
+  var accounts;
+  useEffect(() => {
+        //  Create WalletConnect Provider
+      (async()=>{
+        const provider = new WalletConnectProvider({
+          rpc: {
+            1: "https://mainnet.mycustomnode.com",
+            3: "https://ropsten.mycustomnode.com",
+            100: "https://dai.poa.network",
+            // ...
+          },
+        });
+        
+        //  Enable session (triggers QR Code modal)
+        await provider.enable();
+  
+  
+        const web3 = new Web3(provider);
+  
+              // Subscribe to accounts change
+        provider.on("accountsChanged", (accounts: string[]) => {
+          console.log("hi");
+        });
+  
+        // Subscribe to chainId change
+        provider.on("chainChanged", (chainId: number) => {
+          console.log(chainId);
+        });
+  
+        // Subscribe to session disconnection
+        provider.on("disconnect", (code: number, reason: string) => {
+          console.log(code, reason);
+        });
+  
+  
+  //   const accounts = await web3.eth.getAccounts();
+  
+  //  //Get Chain Id
+  // const chainId = await web3.eth.chainId();
+
+  // //  Get Network Id
+  // const networkId = await web3.eth.net.getId();
+  
+  // // Send Transaction
+  // const txHash = await web3.eth.sendTransaction();
+  
+  // // Sign Transaction
+  // const signedTx = await web3.eth.signTransaction();
+  
+  // // Sign Message
+  // const signedMessage = await web3.eth.sign();
+  
+  // // Sign Typed Data
+  // const signedTypedData = await web3.eth.signTypedData();
+  
+      })()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       React
     </div>
   );
 }
